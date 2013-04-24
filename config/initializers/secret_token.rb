32e5b1a1217c91bf248a02cc094a42526958cd4b
Fernando Paredes <nanoid.xd@gmail.com>
2013-04-23 21:16:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdfund::Application.config.secret_key_base = 'bf72b70ea9a377af2e0661cc0472856917e0815d99b049c3ffe63efc8d15eb92113378f8f78b94239472fe7a05ef2527d2eb2ee97359bbdaee7026b0e72e3584'
